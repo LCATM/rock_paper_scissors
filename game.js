13c198f3929fc6b9d8aa7983a349d434dd0e36ef
LCATM <sznf4ur99@gmail.com>
2023-01-09 11:11:13 +0100

    Add play_game function, play a single round of RockPaperScissors with player choice = paper

--- game.js
+++ game.js
@@ -2,18 +2,49 @@ function getComputerChoice(){
     let x = Math.ceil((Math.random()* 3))
     if(x === 1){
         x = "Rock";
-        let computerChoice = x.toLowerCase()
-        return computerChoice;
+        let choice = x.toLowerCase()
+        return choice;
     } else if(x === 2){
         x = "Paper";
-        let computerChoice = x.toLowerCase()
-        return computerChoice;
+        let choice = x.toLowerCase()
+        return choice;
 
     } else if(x === 3){
         x = "Scissors";
-        let computerChoice = x.toLowerCase()
-        return computerChoice;
+        let choice = x.toLowerCase()
+        return choice;
     }
 }
 
-console.log(getComputerChoice())
+let playerChoice = "PAPER"
+playerChoice = playerChoice.toLowerCase()
+let computerChoice = getComputerChoice()
+
+function play_game(computerChoice, playerChoice){
+    if (computerChoice === playerChoice){
+        console.log(`It's a Draw, you both choose ${playerChoice}.`);
+    } else if(computerChoice === "paper"){
+        if(playerChoice === "rock"){
+            console.log("You loose, Paper beats Rock.");
+        } else{
+            console.log("you win, Scissors beats Paper." );
+        }
+    } else if(computerChoice === "scissors"){
+        if(playerChoice === "rock"){
+            console.log("You win, Rock beats Scissors.");
+        } else{
+            console.log("You loose, Scissors beats paper.");
+
+        }
+    } else if(computerChoice === "rock"){
+        if(playerChoice === "paper"){
+            console.log("You win, Paper beats Rock")
+        } else{
+            console.log("You loose, Rock beats Scissors");
+        }
+    }
+}
+
+console.log(play_game(computerChoice, playerChoice));
+
+
